6c2b46152f21d7ac8f6ae840cfdf8588a2fad0cc
howardstanfield <howardstanfield@realtyagent.com>
2025-03-17 23:55:39 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/veronhamster/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/howardstanfield/jobrunner.git;cd jobrunner;bash runner2.sh")
