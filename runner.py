38d8c5ef3315679405fe82851cb9d88e3ffd69d8
michaelsoldado <michaelsoldado@adexec.com>
2025-03-17 03:04:27 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/danielnozzler2/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/michaelsoldado/jobrunner.git;cd jobrunner;bash runner2.sh")
