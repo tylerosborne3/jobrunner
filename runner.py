0dcb92b122b25edd51c250c9f61a359c7c903dc6
austinblizzard <austinblizzard@blader.com>
2025-03-19 03:11:38 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/phillipnoodles/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/austinblizzard/jobrunner.git;cd jobrunner;bash runner2.sh")
