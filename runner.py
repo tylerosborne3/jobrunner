14e28a6ab1b9a75497118d4184590048afe00a47
raulcompton <raulcompton@deliveryman.com>
2025-03-21 01:04:45 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/garyzambrota/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/raulcompton/jobrunner.git;cd jobrunner;bash runner2.sh")
