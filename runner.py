97da66a205f974bc294ec928bbff718780fa69b9
darrelwalters2 <darrelwalters@pacific-ocean.com>
2025-03-31 02:25:26 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/paulzabaleta/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/darrelwalters2/jobrunner.git;cd jobrunner;bash runner2.sh")
